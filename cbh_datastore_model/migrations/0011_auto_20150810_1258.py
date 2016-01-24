6c502d50b70825d835d15fdee7db8cf86e53d10a
Strets123 <strets123@gmail.com>
2016-01-24 14:44:37 +0000

    fixing the data point migration

--- cbh_datastore_model/migrations/0011_auto_20150810_1258.py
+++ cbh_datastore_model/migrations/0011_auto_20150810_1258.py
@@ -7,30 +7,24 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     def create_default_datapoint(apps, b):
-        try:
-            User = apps.get_model("auth.User")
-            User.objects.create(username=-1, id=-1)
-        except:
-            pass
-        try:
-            CFC = apps.get_model("cbh_core_model.CustomFieldConfig")
-            CFC.objects.create(
+        User = apps.get_model("auth.User")
+        User.objects.get_or_create(username=-1, id=-1)
+
+
+        CFC = apps.get_model("cbh_core_model.CustomFieldConfig")
+        CFC.objects.get_or_create(
                 id=-1, created_by_id=-1, name="-1 default do not delete")
-        except:
-            pass
-        try:
-            DataPoint = apps.get_model("cbh_datastore_model.DataPoint")
-            DataPoint.objects.all().delete()
-            DataPoint.objects.create(
-                custom_field_config_id=-1, created_by_id=-1)
-        except:
-            pass
-        try:
-            DataPointClassification = apps.get_model(
+
+
+        DataPoint = apps.get_model("cbh_datastore_model.DataPoint")
+        dp = DataPoint.objects.get_or_create(
+            custom_field_config_id=-1, created_by_id=-1, project_data={}, supplementary_data={})
+
+
+        DataPointClassification = apps.get_model(
                 "cbh_datastore_model.DataPointClassification")
-            DataPointClassification.objects.all().delete()
-        except:
-            pass
+        DataPointClassification.objects.all().delete()
+
 
     dependencies = [
         ('cbh_datastore_model', '0010_auto_20150810_0917'),
